db9477a49be838425504a2c6211936c0911b178a
David McElroy <d.mcelroy@uel.ac.uk>
2014-07-11 16:06:45 +0100

    style the requesta copy button

--- cfg/themes/roar/static/roar_template/Scripts/main.js
+++ cfg/themes/roar/static/roar_template/Scripts/main.js
@@ -19,6 +19,7 @@ j(document).ready(function () {
                 //j('#ep_phraseedit_docklands_btn_radius').click(previewCSS);
                 j('.docklands_admin_widget input[type="button"][value="Preview"]').click(previewCSS);
 
+		j('a:contains("Request a copy")').addClass('btn');
 
         });
 
