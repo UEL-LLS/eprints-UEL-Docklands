f74606787edb1aac1e782a72f5b78580dfa3677c
David McElroy <d.mcelroy@uel.ac.uk>
2014-07-14 16:30:33 +0100

    remove icon replacement from latest_tool

--- cfg/themes/roar/static/roar_template/Scripts/main.js
+++ cfg/themes/roar/static/roar_template/Scripts/main.js
@@ -5,9 +5,9 @@ j(document).ready(function () {
 	//j('.panel-collapse').parent('.panel').children('.panel-heading').children('.panel-title').children('a').append('<span class="glyphicon glyphicon-plus"></span>');
 
 	//replace the img icons with nice font awesome ones
-	j('.ep_doc_icon[src$="fileicons/application_msword.png"]').replaceWith('<i class="fa fa-file-word-o fa-5x"></i>');
+	j('.panel-docklands-prime .ep_doc_icon[src$="fileicons/application_msword.png"]').replaceWith('<i class="fa fa-file-word-o fa-5x"></i>');
 	j('.ep_doc_icon[src$="fileicons/application_pdf.png"]').replaceWith('<i class="fa fa-file-pdf-o fa-5x"></i>');
-	j('.ep_doc_icon[src$="fileicons/text.png"]').replaceWith('<i class="fa fa-file-text-o fa-5x"></i>');
+	j('.panel-docklands-prime .ep_doc_icon[src$="fileicons/text.png"]').replaceWith('<i class="fa fa-file-text-o fa-5x"></i>');
 	j('.ep_doc_icon[src$="fileicons/other.png"]').replaceWith('<i class="fa fa-file-o fa-5x"></i>');
 	j('.ep_doc_icon[src$="fileicons/unknown.png"]').replaceWith('<i class="fa fa-file-o fa-5x"></i>');
 	j('.ep_doc_icon[src$="fileicons/archive.png"]').replaceWith('<i class="fa fa-file-zip-o fa-5x"></i>');
