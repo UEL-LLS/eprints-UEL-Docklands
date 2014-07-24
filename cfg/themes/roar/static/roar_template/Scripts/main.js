b3dbb93bed37374dcbc062ba12f7347fbc0c7f53
David McElroy <d.mcelroy@uel.ac.uk>
2014-07-24 15:22:59 +0100

    trying match

--- cfg/themes/roar/static/roar_template/Scripts/main.js
+++ cfg/themes/roar/static/roar_template/Scripts/main.js
@@ -69,7 +69,7 @@ j(document).ready(function () {
 	//var pathname = window.location.pathname;
 	var pathname = j(location).attr("href");
 	
-	if (pathname == "http://roartest.uel.ac.uk$" || pathname == "http://ueltest.da.ulcc.ac.uk$" || pathname == "http://roar.uel.ac.uk$")  { 
+	if (pathname.match("http://roartest.uel.ac.uk/$") || pathname.match("http://ueltest.da.ulcc.ac.uk/$") || pathname.match("http://roar.uel.ac.uk/$"))  { 
                 console.log("Roar: "+pathname);
 		j('.docklands-modal-header').addClass("data-bg");
 	}
