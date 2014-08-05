26cde239c768b08bcba6464f08fa0813d99b2d86
David McElroy <d.mcelroy@uel.ac.uk>
2014-08-05 14:49:24 +0100

    small update to a description and menu.js update to makename look better

--- cfg/themes/neutral/static/neutral_template/Scripts/menu.js
+++ cfg/themes/neutral/static/neutral_template/Scripts/menu.js
@@ -11,5 +11,7 @@ j(document).ready(function () {
 	j('#ep_tm_menu_tools, .ep_tm_key_tools').addClass('nav nav-pills nav-stacked');
 	//menu manipluation and collapse stuff
 	j('.ep_tm_key_tools').removeClass('ep_tm_key_tools').addClass('navbar-collapse collapse').attr('id','ep_menu');
+	
+	j('nav #ep_menu li').wrapInner('<a style="cursor:default" href="#"></a>');
 
 });
