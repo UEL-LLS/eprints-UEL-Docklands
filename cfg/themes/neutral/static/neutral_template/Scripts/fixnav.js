a5524a2fb6341242cfd3931df6e76d9969a01795
David McElroy <d.mcelroy@uel.ac.uk>
2014-08-05 16:20:53 +0100

    fixed fixed menu in neutral

--- cfg/themes/neutral/static/neutral_template/Scripts/fixnav.js
+++ cfg/themes/neutral/static/neutral_template/Scripts/fixnav.js
@@ -7,7 +7,7 @@ var j = jQuery.noConflict();
 j(document).ready(function () {
 
                 j(window).scroll(function() {
-                        if (j(this).scrollTop() > 51) {
+                        if (j(this).scrollTop() > 51 && j(window).width() > 750) {
                         	j('#ep_internal_nav').addClass('navbar-fixed-top').css("opacity","0.95");
 			}else {
                         	j('#ep_internal_nav').removeClass('navbar-fixed-top').css("opacity","1");
