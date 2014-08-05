1f26920a308383c6ed640e4dcbc79ed761ec3697
David McElroy <d.mcelroy@uel.ac.uk>
2014-08-05 14:22:54 +0100

    moved adminscript stuff to cfd.d file and updated template to match

--- cfg/cfg.d/xxx_docklands.pl
+++ cfg/cfg.d/xxx_docklands.pl
@@ -4,6 +4,20 @@ var j = jQuery.noConflict();
 j(document).ready(function () {
         //j('#ep_phraseedit_docklands_btn_radius').click(previewCSS);
         j('.docklands_admin_widget input[type="button"][value="Preview"]').click(previewCSS);
+
+	//Theme Descriptions
+        var desc = j('#theme_select option:selected').data('description');
+        j('#theme_description').html("<p>"+desc+"</p>");
+        j( "#theme_select" ).change(function() {
+                var desc = j('#theme_select option:selected').data('description');
+                j('#theme_description').html("<p>"+desc+"</p>");
+        });
+
+        j('.ep_phraseedit_widget').click(function() {
+                j('.docklands_admin_widget form').addClass('row');
+                j('.docklands_admin_widget form textarea').addClass('col-md-12');
+                j(':not(#docklands_theme).docklands_admin_widget form input').addClass('col-md-3');
+        });
 });
 
 function previewCSS() {
