96f008825ce34d1038d73425d0a372df4b083c92
David McElroy <d.mcelroy@uel.ac.uk>
2014-08-11 11:38:37 +0100

    changes to make the eprints workflow nicer

--- cfg/themes/roar/static/roar_template/Scripts/main.js
+++ cfg/themes/roar/static/roar_template/Scripts/main.js
@@ -138,6 +138,15 @@ j(document).ready(function () {
         j('.panel-card > span:nth-child(2)').addClass('col-md-8');
         j('.panel-card > span:nth-child(3)').addClass('col-md-3').attr('align','right');
 
+
+	//Make panels in workflow stages
+	j('.ep_form_field_input .ep_sr_component').addClass('panel panel-default').removeClass('ep_sr_compenent');
+	j('.ep_form_field_input .ep_sr_title_bar').addClass('panel-heading').removeClass('ep_sr_title_bar');
+	j('.ep_form_field_input .ep_sr_content').addClass('panel-body').removeClass('ep_sr_content');
+	//j('.panel .panel-body table.ep_form_input_grid').addClass('table');
+	j('.panel table.table th').css('border','none');
+	j('.panel table.table td.ep_form_input_grid_arrows').css('border-bottom','1px solid #ddd');
+
 });
 
 
